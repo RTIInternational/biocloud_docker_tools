45c2dde6ad0493dbe5cf0716680e179a91855ed5
Nathan <ngaddis@ip-172-31-25-191.ec2.internal>
2020-10-22 17:17:29 +0000

    Added convert_seq_gds_to_snp_gds

---
+++ convert_seq_gds_to_snp_gds/v1/convert_seq_gds_to_snp_gds.R
@@ -0,0 +1,53 @@
+#!/usr/local/bin/Rscript
+
+library(SeqArray)
+
+args <- commandArgs(TRUE)
+
+loop = TRUE
+fileInSeqGds = ""
+fileOutSnpGds = ""
+compressGeno = "ZIP_RA"
+optimize = FALSE
+dosage = FALSE
+
+while (loop) {
+
+	if (args[1] == "--in-bgen") {
+		fileInSeqGds = args[2]
+	}
+
+	if (args[1] == "--out-gds") {
+		fileOutSnpGds = args[2]
+	}
+
+	if (args[1] == "--compress-geno") {
+		compressGeno = args[2]
+	}
+
+	if (args[1] == "--optimize") {
+		optimize = TRUE
+	}
+
+	if (args[1] == "--dosage") {
+		dosage = TRUE
+	}
+
+	if (length(args) > 1) {
+		args = args[2:length(args)]
+	} else {
+		loop=FALSE
+	}
+
+}
+
+seqGDS2SNP(
+    fileInSeqGds,
+    fileOutSnpGds,
+    compress.geno=compressGeno,
+    compress.annotation=compressGeno,
+    optimize=optimize,
+    verbose=TRUE,
+    dosage=dosage
+)
+
