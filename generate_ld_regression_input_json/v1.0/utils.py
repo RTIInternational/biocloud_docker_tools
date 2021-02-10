f35b56b06449db03a2206906dd8608a1d180c8c6
alexwaldrop <alex.waldrop.jr@gmail.com>
2021-02-10 16:10:34 -0500

    Added cmd line tool for generating ld regression input

---
+++ generate_ld_regression_input_json/v1.0/utils.py
@@ -0,0 +1,82 @@
+import sys
+import logging
+import argparse
+import os
+
+def get_argparser():
+    # Configure and return argparser object for reading command line arguments
+    argparser_obj = argparse.ArgumentParser(prog="generate_ld_regression_input_json")
+
+    def file_type(arg_string):
+        """
+        This function check both the existance of input file and the file size
+        :param arg_string: file name as string
+        :return: file name as string
+        """
+        if not os.path.exists(arg_string):
+            err_msg = "%s does not exist! " \
+                      "Please provide a valid file!" % arg_string
+            raise argparse.ArgumentTypeError(err_msg)
+
+        return arg_string
+
+    # Path to VCF input file
+    argparser_obj.add_argument("--json-input",
+                               action="store",
+                               type=file_type,
+                               dest="json_input",
+                               required=True,
+                               help="Path to base WDL input where additional phenotype data will be added.")
+
+    # Path to VCF input file
+    argparser_obj.add_argument("--pheno-file",
+                               action="store",
+                               type=file_type,
+                               dest="pheno_file",
+                               required=True,
+                               help="Path to Excel-formatted spreadsheet containing additional phenotypes to compare using LD-Regression.")
+    # Verbosity level
+    argparser_obj.add_argument("-v",
+                               action='count',
+                               dest='verbosity_level',
+                               required=False,
+                               default=0,
+                               help="Increase verbosity of the program."
+                                    "Multiple -v's increase the verbosity level:\n"
+                                    "0 = Errors\n"
+                                    "1 = Errors + Warnings\n"
+                                    "2 = Errors + Warnings + Info\n"
+                                    "3 = Errors + Warnings + Info + Debug")
+
+    return argparser_obj
+
+def configure_logging(verbosity):
+    # Setting the format of the logs
+    FORMAT = "[%(asctime)s] %(levelname)s: %(message)s"
+
+    # Configuring the logging system to the lowest level
+    logging.basicConfig(level=logging.DEBUG, format=FORMAT, stream=sys.stderr)
+
+    # Defining the ANSI Escape characters
+    BOLD = '\033[1m'
+    DEBUG = '\033[92m'
+    INFO = '\033[94m'
+    WARNING = '\033[93m'
+    ERROR = '\033[91m'
+    END = '\033[0m'
+
+    # Coloring the log levels
+    if sys.stderr.isatty():
+        logging.addLevelName(logging.ERROR, "%s%s%s%s%s" % (BOLD, ERROR, "ERROR", END, END))
+        logging.addLevelName(logging.WARNING, "%s%s%s%s%s" % (BOLD, WARNING, "WARNING", END, END))
+        logging.addLevelName(logging.INFO, "%s%s%s%s%s" % (BOLD, INFO, "INFO", END, END))
+        logging.addLevelName(logging.DEBUG, "%s%s%s%s%s" % (BOLD, DEBUG, "DEBUG", END, END))
+    else:
+        logging.addLevelName(logging.ERROR, "ERROR")
+        logging.addLevelName(logging.WARNING, "WARNING")
+        logging.addLevelName(logging.INFO, "INFO")
+        logging.addLevelName(logging.DEBUG, "DEBUG")
+
+    # Setting the level of the logs
+    level = [logging.ERROR, logging.WARNING, logging.INFO, logging.DEBUG][verbosity]
+    logging.getLogger().setLevel(level)
