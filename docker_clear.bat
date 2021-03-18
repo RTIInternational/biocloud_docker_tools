dd5afb96adffe218f89d3b8f69bde07067457c08
Chris Ball <christopherball@rti.org>
2021-03-18 14:53:34 -0400

    Move from rticode

---
+++ docker_clear.bat
@@ -0,0 +1,4 @@
+:: Delete all images on machine
+@echo off
+FOR /f "tokens=*" %%i IN ('docker ps -aq') DO docker rm %%i
+FOR /f "tokens=*" %%i IN ('docker images --format "{{.ID}}"') DO docker rmi %%i
