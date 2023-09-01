5cc7335131dbc3339271e9120191968a363dc040
David Williams <dnwillia23@gmail.com>
2023-09-01 19:53:31 +0000

    Added extraction of Standard Deviation and per base %N

--- multiqc_extract/v1.0/extract_avg_per_base_seq_quality.r
+++
@@ -1,5 +0,0 @@
-# Used to get the average per base sequence quality after 30 cycles
-args<-commandArgs(TRUE)
-dat = t(read.table(args[1],row.names = 1))
-out_dat = mean(dat[dat[,1] >= 30,2])
-print(out_dat)
--- multiqc_extract/v1.0/unzip_extract_stats.sh
+++ multiqc_extract/v1.0/unzip_extract_stats.sh
@@ -8,14 +8,6 @@ else
   echo "Ding!  Found zip file.  Proceeding with extraction script"
 fi
 
-if [ ! -f output.csv ]
-then
-    echo "Output file does not exist. Initializing..."
-    echo "sample_name,total_sequences,read_length,max_per_sequence_quality_scores,average_per_base_seq,average_sequence_duplication_levels" > output.csv
-else
-    echo "Output file found.  Appending results to the end"
-fi
-
 if [ ! -d output_dir ]
 then
   mkdir ./output_dir
@@ -31,6 +23,15 @@ echo "Sample name for this run: $sample_name"
 total_sequences=$(jq .report_general_stats_data[1].$sample_name.total_sequences $file_to_read)
 read_length=$(jq .report_general_stats_data[1].$sample_name.avg_sequence_length $file_to_read)
 
+output_file_name="${sample_name:1:-1}_output.csv"
+if [ ! -f $output_file_name ]
+then
+    echo "Output file does not exist. Initializing..."
+    echo "sample_name,total_sequences,read_length,max_per_sequence_quality_scores,per_base_seq,sd_per_base_seq,sequence_duplication_levels,sd_sequence_duplication_levels,average_per_base_n_content,sd_per_base_n_content" > $output_file_name
+else
+    echo "Output file found for ${sample_name}.  Appending results to the end"
+fi
+
 # Highest Per sequence quality scores
 echo "Extracting for Highest Per sequence quality scores"
 per_sequence_quality_scores_file=$(find ./output_dir -maxdepth 1 -name '*per_sequence_quality_scores*.txt')
@@ -41,16 +42,22 @@ max_per_sequence_quality_scores=$(Rscript extract_max_per_seq_quality_score.r $p
 echo "Extracting for Average Per base sequence content after 30 cycles"
 per_base_seq_quality_file=$(find ./output_dir -maxdepth 1 -name '*per_base_sequence_quality*.txt')
 echo "Reading from $per_base_seq_quality_file"
-average_per_base_seq=$(Rscript extract_avg_per_base_seq_quality.r $per_base_seq_quality_file)
+per_base_seq=$(Rscript extract_per_base_seq_quality.r $per_base_seq_quality_file)
 
 # Average Sequence duplication levels as percentage of duplicates
 echo "Extracting for Average Sequence duplication levels as percentage of duplicates"
 sequence_duplication_levels_file=$(find ./output_dir -maxdepth 1 -name '*sequence_duplication_levels*.txt')
 echo "Reading from $sequence_duplication_levels_file"
-average_sequence_duplication_levels=$(Rscript extract_avg_seq_duplication_level.r $sequence_duplication_levels_file)
+sequence_duplication_levels=$(Rscript extract_seq_duplication_level.r $sequence_duplication_levels_file)
 
-# Appending to output.csv
-echo "$sample_name,$total_sequences,$read_length,${max_per_sequence_quality_scores:4},${average_per_base_seq:4},${average_sequence_duplication_levels:4}" >> output.csv
+# Average Sequence duplication levels as percentage of duplicates
+echo "Extracting for Average Sequence duplication levels as percentage of duplicates"
+per_base_n_content_file=$(find ./output_dir -maxdepth 1 -name '*per_base_n_content*.txt')
+echo "Reading from $per_base_n_content_file"
+per_base_n_content=$(Rscript extract_per_base_n_content.r $per_base_n_content_file)
+
+# Appending to output CSV file
+echo "$sample_name,$total_sequences,$read_length,${max_per_sequence_quality_scores:4},${per_base_seq:5:-1},${sequence_duplication_levels:5:-1},${per_base_n_content:5:-1}" >> $output_file_name
 
 # Removing the unzipped directory
-rm -rf ./output_dir
+# rm -rf ./output_dir
