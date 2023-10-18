24efd0b1f4720abe5926d5df7cf2f49fad1e85f4
Jesse Marks <32715488+jaamarks@users.noreply.github.com>
2023-10-18 16:39:31 -0400

    trivial update to test github actions build process

--- multiqc_extract/v1.0/unzip_extract_stats.R
+++ multiqc_extract/v1.0/unzip_extract_stats.R
@@ -63,9 +63,9 @@ for (zip_file in zip_files){
   seq_dup_level <- extract_seq_duplication_level(sequence_duplication_levels_file)
   per_base_n_content <- extract_per_base_n_content(per_base_n_content_file)
   
-  ###############################
+  ##############################
   # WRITE/APPEND TO CSV
-  ###############################
+  ##############################
   
   output_data <- c(zip_file, sample_name, total_sequences, read_length, max_per_seq_qual, per_base_seq_qual, seq_dup_level, per_base_n_content)
   column_names <- c("zip_file","sample_name","total_sequences","read_length","max_per_sequence_quality_scores","per_base_seq","sd_per_base_seq","sequence_duplication_levels","sd_sequence_duplication_levels","average_per_base_n_content","sd_per_base_n_content")
