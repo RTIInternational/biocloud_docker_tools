82ccc6adba98f8f8128acce28f5c770b29018d91
David Williams <dnwillia23@gmail.com>
2024-01-02 22:58:08 +0000

    Updated multiqc_extract to pull files individually from ZIP

--- multiqc_extract/v1.0/unzip_extract_stats.R
+++ multiqc_extract/v1.0/unzip_extract_stats.R
@@ -28,6 +28,7 @@ spec <- matrix(c(
 # Parse the command line parameters into R
 args=getopt(spec, argString)
 
+
 # If missing required fields then display usage and quit
 if ( !is.null(args$help)) {
   cat(usage)
@@ -52,8 +53,8 @@ if(!grepl("\\.csv$",args$outfile)){
   args$outfile <- paste0(args$outfile,".csv")
   }
 
-zip_files <- list.files(path = args$inputpath, pattern = "multiqc_data\\.zip")
-fastq_files <- list.files(path = args$inputpath, pattern = "\\.fastq$")
+zip_files <- list.files(path = args$inputpath, pattern = "multiqc_data\\.zip", full.names = TRUE)
+fastq_files <- list.files(path = args$inputpath, pattern = "\\.fastq$", , full.names = TRUE)
 
 if(length(zip_files)==0){
   stop("No MultiQC ZIP files found. Exiting")
@@ -65,14 +66,12 @@ if(length(zip_files) != length(fastq_files)){
 
 for (zip_file in zip_files){
   tmp_unzip_dir_name <- paste0(args$inputpath, "/output_dir")
-  dir.create(tmp_unzip_dir_name)
-  unzip(zip_file,exdir=tmp_unzip_dir_name, overwrite=TRUE)
   
   ############################################
   # EXTRACTING FROM MULTIQC JSON OUTPUT FILE
   ############################################
-  
-  result <- fromJSON(txt = paste0(tmp_unzip_dir_name,"/multiqc_data.json"))
+
+  result <- fromJSON(unzip(zip_file,"multiqc_data.json"))
   sample_name <- names(result$report_data_sources$FastQC$all_sections)
   total_sequences <- na.omit(result$report_general_stats_data[[sample_name]]$total_sequences)[1]
   read_length <- na.omit(result$report_general_stats_data[[sample_name]]$avg_sequence_length)[1]
@@ -82,38 +81,39 @@ for (zip_file in zip_files){
   ############################################################################
   
   multiqc_files <- list.files(path = tmp_unzip_dir_name, recursive = TRUE, full.names = TRUE)
-  
-  per_sequence_quality_scores_file <- multiqc_files[grepl('per_sequence_quality_scores.*.txt',multiqc_files)]
-  per_base_seq_quality_file <- multiqc_files[grepl('per_base_sequence_quality.*.txt',multiqc_files)]
-  sequence_duplication_levels_file <- multiqc_files[grepl('sequence_duplication_levels.*.txt',multiqc_files)]
-  per_base_n_content_file <- multiqc_files[grepl('per_base_n_content.*.txt',multiqc_files)]
-  
+
+  per_sequence_quality_scores_file <- "mqc_fastqc_per_sequence_quality_scores_plot_1.txt"
+  per_base_seq_quality_file <- "mqc_fastqc_per_base_sequence_quality_plot_1.txt"
+  sequence_duplication_levels_file <- "mqc_fastqc_sequence_duplication_levels_plot_1.txt"
+  per_base_n_content_file <- "mqc_fastqc_per_base_n_content_plot_1.txt"
+
   extract_max_per_seq_quality_score <- function(input_file){
-    dat = t(read.table(input_file,row.names = 1))
+    dat = t(read.table(unzip(zip_file,input_file),row.names = 1))
     out_dat = max(dat[,2])
     return(out_dat)
   }
   
   extract_per_base_seq_quality <- function(input_file){
-    dat = t(read.table(input_file,row.names = 1))
+    dat = t(read.table(unzip(zip_file,input_file),row.names = 1))
     out_dat_mean = mean(dat[dat[,1] >= 30,2])
     out_dat_sd = sd(dat[dat[,1] >= 30,2])
     return(c(out_dat_mean,out_dat_sd))
   }
   
   extract_seq_duplication_level <- function(input_file){
-    dat = t(read.table(input_file,row.names = 1, fill = TRUE))
+    dat = t(read.table(unzip(zip_file,input_file),row.names = 1, fill = TRUE))
     out_dat_mean = mean(as.numeric(as.vector(dat[!is.na(dat[,2]) & nchar(dat[,2])>0,2])))
     out_dat_sd = sd(as.numeric(as.vector(dat[!is.na(dat[,2]) & nchar(dat[,2])>0,2])))
     return(c(out_dat_mean,out_dat_sd))
   }
   
   extract_per_base_n_content <- function(input_file){
-    dat = t(read.table(input_file,row.names = 1))
+    dat = t(read.table(unzip(zip_file,input_file),row.names = 1))
     out_dat_mean = mean(dat[,2])
     out_dat_sd = sd(dat[,2])
     return(c(out_dat_mean,out_dat_sd))
-  }
+  }    
+    
   
   max_per_seq_qual <- extract_max_per_seq_quality_score(per_sequence_quality_scores_file)
   per_base_seq_qual <- extract_per_base_seq_quality(per_base_seq_quality_file)
@@ -130,9 +130,9 @@ for (zip_file in zip_files){
   ###############################
   # WRITE/APPEND TO CSV
   ###############################
-  
   output_data <- c(max_row + 1,zip_file, sample_name, total_sequences, read_length, max_per_seq_qual, per_base_seq_qual, seq_dup_level, per_base_n_content)
   column_names <- c("row_number","zip_file","sample_name","total_sequences","read_length","max_per_sequence_quality_scores","per_base_seq","sd_per_base_seq","sequence_duplication_levels","sd_sequence_duplication_levels","average_per_base_n_content","sd_per_base_n_content")
+
   
   row <- data.frame(matrix(ncol = length(column_names), nrow=0))
   colnames(row) <- column_names
