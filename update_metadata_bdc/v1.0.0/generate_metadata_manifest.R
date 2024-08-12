87fb0c4c0f5cf12e35a754b462b9c907264385fc
menger5 <143822465+menger5@users.noreply.github.com>
2024-08-12 15:34:09 -0400

    Create generate_metadata_manifest.R
    Initial commit

---
+++ update_metadata_bdc/v1.0.0/generate_metadata_manifest.R
@@ -0,0 +1,284 @@
+#-----------------------------------------------------
+# Description: 
+# This script utilizes the SevenBridges 
+# API to generate a metadata manifest file for all
+# files within a specified project. This manifest
+# can then be used to update all files' metadata using
+# the "Update Metadata using Manifest File" feature
+# on the BDC user interface.
+# 
+# 
+# Developer: Mike Enger
+# Project: 
+# Date: 12AUG2024
+#
+#
+# Revisions
+# v1.0 initial commit
+#
+#-----------------------------------------------------
+
+if(!require('getopt')){install.packages('getopt', dependencies = T); library(getopt)}
+if(!require('dplyr')){install.packages('dplyr', dependencies = T); library(dplyr)}
+if(!require('httr')){install.packages('httr', dependencies = T); library(httr)}
+if(!require('stringr')){install.packages('stringr', dependencies = T); library(stringr)}
+if(!require('lubridate')){install.packages('lubridate', dependencies = T); library(lubridate)}
+if(!require('sevenbridges2')){install.packages('sevenbridges2', dependencies = T); library(sevenbridges2)}
+
+#-----------------------------------------------------
+# Setup global arguments and command line use
+#-----------------------------------------------------
+
+# Define usage message
+usage <- paste("Usage: script_name.r
+             -- Required Parameters --
+              [-t | --token]         <API Token> (Required)
+              [-p | --project_id]    <Project ID> (Required)
+              [-o | --output_path]   <Path to save the output file> (Required)
+             -- Help Flag --
+              [-h | --help]          <Displays this help message>
+             Example:
+             script_name.r -t your_token -p project_id -o /path/to/output
+              \n", sep="")
+
+# Specify command-line arguments
+spec <- matrix(c(
+  'token',        't', 1, "character",
+  'project_id',   'p', 1, "character",
+  'output_path',  'o', 1, "character",
+  'help',         'h', 0, "logical"
+), byrow=TRUE, ncol=4)
+
+# Parse command-line arguments
+args <- getopt(spec)
+
+# Display help message if needed
+if (!is.null(args$help) || is.null(args$token) || is.null(args$project_id) || is.null(args$output_path)) {
+  cat(usage)
+  q(status = 1)
+}
+
+# Assign arguments to variables
+token <- args$token
+project_id <- args$project_id
+output_path <- args$output_path
+
+
+
+#-----------------------------------------------------
+# Setup logging
+#-----------------------------------------------------
+
+add_to_log <- function(lvl, func, message){
+  timestamp <- paste0("[", Sys.time(), "]")
+  entry <- paste(timestamp, func, toupper(lvl), message, sep = " - ")
+  cat(paste0(entry, "\n"))
+}
+
+
+
+#-----------------------------------------------------
+# Required Functions and Dataframes
+#-----------------------------------------------------
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Initialize file and folder dataframes
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+df_files_empty <- data.frame(
+  id = character(0),
+  name = character(0),
+  upload_directory = character(0),
+  upload_directory_id = character(0),
+  size = integer(0),
+  project = character(0),
+  type = character(0)
+)
+
+df_folders_empty <- data.frame(
+  id = character(0),
+  name = character(0),
+  parent_id = character(0),
+  parent_name = character(0)
+)
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Function to extract file and folder info
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+add_to_log("info", "setup", "Setting up recursive file and folder extraction function")
+extract_file_folder_info <- function(project_id, parent_id=NA, parent_name="root", df_files, df_folders) {
+  
+  # initializing offset and combined subdirectory file listing
+  offset <- 0
+  subdirectory_items_list <- list()
+  
+  # Make an initial call to the project/directory to see if there are any files
+  if (nchar(parent_id) == 0){
+    subdirectory_items <- a$files$query(project = project_id, limit = 100, offset = offset)
+  } else{
+    subdirectory_items <- a$files$query(parent = parent_id, limit = 100, offset = offset)
+  }
+  
+  # Loop to get all files in current directory
+  while (length(subdirectory_items$items) > 0) {
+    if (nchar(parent_id) == 0){
+      subdirectory_items <- a$files$query(project = project_id, limit = 100, offset = offset)
+    } else{
+      subdirectory_items <- a$files$query(parent = parent_id, limit = 100, offset = offset)
+    }
+    subdirectory_items_list <- c(subdirectory_items_list,subdirectory_items$items)
+    offset <- offset + length(subdirectory_items$items)
+  }
+  
+  for(item in subdirectory_items_list){
+    
+    if (item$type == "file"){
+      add_to_log("info", "extract", paste("Processing file:", item$name))
+      file_data_row <- data.frame(
+        id = item$id,
+        name = item$name,
+        upload_directory = parent_name,
+        upload_directory_id = parent_id,
+        size = ifelse(is.null(item$size), 0, item$size),
+        project = item$project,
+        type = item$type
+      )
+      df_files <- bind_rows(df_files,file_data_row)
+    }
+    
+    # Extracting folder info AND going into folder to extract subdirectory and file info
+    if (item$type == "folder") {
+      add_to_log("info", "extract", paste("Processing folder:", item$name))
+      folder_data_row <- data.frame(
+        id = item$id,
+        name = item$name,
+        parent_id = item$parent,
+        parent_name = parent_name
+      )
+      
+      df_folders <- bind_rows(df_folders, folder_data_row)
+      
+      list_df_files_folders <- extract_file_folder_info(project_id = project_id,
+                                                        parent_id = item$id,
+                                                        parent_name = item$name,
+                                                        df_files,df_folders)
+      df_files <- list_df_files_folders[[1]]
+      df_folders <- list_df_files_folders[[2]]
+    }
+  }
+  return(list(df_files,df_folders))
+}
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Function to create the final name column
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+create_final_name <- function(new_name, parent_name) {
+  if (is.na(parent_name)) {
+    return(new_name)  # If parent_name is NA, return new_name
+  }
+  if (parent_name == "Root") {
+    return(new_name)  # If parent_name is "Root", return new_name
+  }
+  if (startsWith(parent_name, "Root")) {
+    parent_name <- sub("^Root", "", parent_name)  # Remove "Root/" prefix if present
+  }
+  return(paste(parent_name, new_name, sep = "/"))  # Combine parent_name and new_name
+}
+
+
+
+#-----------------------------------------------------
+# Main 
+#-----------------------------------------------------
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Provide required URLs and authenticate
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# URLs required for API calls
+api_endpoint <- "https://api.sb.biodatacatalyst.nhlbi.nih.gov/v2"
+sb_platform_url <- "https://platform.sb.biodatacatalyst.nhlbi.nih.gov/u/"
+
+add_to_log("info", "setup", "Starting script and setting up API endpoint and platform URL")
+
+# Authenticate
+add_to_log("info", "setup", "Authenticating with the API")
+a <- Auth$new(token = token, url = api_endpoint)
+
+# Get a specific project and list its files at the root level
+add_to_log("info", "get_project", "Retrieving project information")
+p <- a$projects$get(id = project_id)
+p_root_folder <- p$get_root_folder()
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Extract file and folder info 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+add_to_log("info", "main", "Starting extraction of files and folders")
+list_df_files_folders_out <- extract_file_folder_info(project_id, p_root_folder$id, "Root", df_files_empty, df_folders_empty)
+df_files <- list_df_files_folders_out[[1]]
+df_folders <- list_df_files_folders_out[[2]]
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Build Metadata Manifest
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Join df_files with df_folders to get the parent directory name
+df_files <- merge(df_files, df_folders[, c("id", "parent_name")], by.x = "upload_directory_id", by.y = "id", all.x = TRUE)
+
+# Create new name column in df_files with upload_directory appended
+# Skip values with upload_directory of Root
+df_files$name <- ifelse(df_files$upload_directory == "Root", 
+                        df_files$name, 
+                        paste(df_files$upload_directory, df_files$name, sep = "/"))
+
+# Apply the create_final_name function to compile complete file name
+df_files$name <- mapply(create_final_name, df_files$name, df_files$parent_name)
+
+# Remove unnecessary columns
+df_files <- df_files[ , !(names(df_files) %in% c("upload_directory_id", "upload_directory","parent_name", "type"))]
+
+# Add columns for possible metadata fields
+df_files <- cbind(
+  df_files,
+  experimental_strategy = "",
+  library_id = "",
+  platform = "",
+  platform_unit_id = "",
+  file_segment_number = "",
+  quality_scale = "",
+  paired_end = "",
+  reference_genome = "",
+  investigation = "",
+  case_id = "",
+  case_uuid = "",
+  gender = "",
+  race = "",
+  ethnicity = "",
+  primary_site = "",
+  disease_type = "",
+  age_at_diagnosis = "",
+  vital_status = "",
+  days_to_death = "",
+  sample_id = "",
+  sample_uuid = "",
+  sample_type = "",
+  aliquote_id = "",
+  aliquot_uuid = "",
+  description = ""
+)
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#  Output final manifest to csv
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+today <- format(Sys.Date(), "%Y%m%d")
+output_file_name <- paste0("manifest_", today, ".csv")
+output_file_path <- file.path(output_path, output_file_name)
+write.csv(df_files, output_file_path, row.names = FALSE)
+
+add_to_log("info", "main", paste("CSV file written to", output_file_path))
+add_to_log("info", "main", "Metadata Manifest generated successfully")
