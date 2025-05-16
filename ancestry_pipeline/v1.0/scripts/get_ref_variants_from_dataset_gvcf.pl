f64f48d7f87ae476c8920216c9dc70976e2462d2
Nathan Gaddis <ngaddis@rti.org>
2025-05-16 16:09:04 +0000

    Removed allele flipping from ancestry pipeline when genotypes derived from gvcf

--- ancestry_pipeline/v1.0/scripts/get_ref_variants_from_dataset_gvcf.pl
+++ ancestry_pipeline/v1.0/scripts/get_ref_variants_from_dataset_gvcf.pl
@@ -1,5 +1,25 @@
 #!/usr/bin/perl
 
+# This script is used to extract variants from a gVCF file based on a reference BIM file.
+# The script assumes that both the gVCF and BIM files are on the same strand.
+# It also assumes that the BIM file does not contain A/T or C/G SNPs.
+# It filters the variants based on quality and genotype quality, and outputs the results in VCF format.
+# The script takes the following command line arguments:
+# --file_in_gvcf: Input gVCF file (can be gzipped)
+# --file_in_ref_bim: Input reference BIM file
+# --file_out_prefix: Output prefix for the VCF and variants files
+# --include_monomorphic_positions: Include monomorphic positions in the output (default: 0)
+# --filter_by_qual: Filter variants by quality (default: 0)
+# --filter_by_gq: Filter variants by genotype quality (default: 0)
+# --hom_gq_threshold: Genotype quality threshold for homozygous variants (default: 99)
+# --het_gq_threshold: Genotype quality threshold for heterozygous variants (default: 48)
+#
+# Usage:
+# perl get_ref_variants_from_dataset_gvcf.pl --file_in_gvcf <input_gvcf> --file_in_ref_bim <input_bim> --file_out_prefix <output_prefix> [options]
+# Example:
+# perl get_ref_variants_from_dataset_gvcf.pl --file_in_gvcf input.gvcf.gz --file_in_ref_bim ref.bim --file_out_prefix output_prefix --include_monomorphic_positions 1 --filter_by_qual 1 --filter_by_gq 1 --hom_gq_threshold 99 --het_gq_threshold 48
+
+
 use strict;
 use warnings;
 use Getopt::Long;
@@ -29,25 +49,16 @@ GetOptions (
     'het_gq_threshold:i' => \$het_gq_threshold
 ) or die("Invalid options");
 
-sub flip {
-    my ($allele) = @_;
-    my $allele_complement = "";
-    my %flipMap = (
-        "A" => "T",
-        "T" => "A",
-        "C" => "G",
-        "G" => "C",
-        "-" => "-"
-    );
-    foreach my $nt (reverse(split("", $allele))) {
-        if (!exists($flipMap{uc($nt)})) {
-            $allele_complement = $allele;
-            last;
-        } else {
-            $allele_complement .= $flipMap{uc($nt)};
-        }
-    }
-    return $allele_complement;
+sub add_variant_to_output {
+    my %opt = %{ shift @_ };
+    my @F = @{$opt{F}};
+    my $OUT_VCF = $opt{OUT_VCF};
+    my $OUT_VARIANTS = $opt{OUT_VARIANTS};
+    $F[7] = "";
+    $F[8] =~ s/:.+//;
+    $F[9] =~ s/:.+//;
+    print $OUT_VCF join("\t", @F)."\n";
+    print $OUT_VARIANTS $F[2]."\n";
 }
 
 my %variants = ();
@@ -68,7 +79,6 @@ close REF_BIM;
 # Get dataset variants by position
 open(OUT_VCF, "> ".$file_out_prefix.".vcf");
 open(OUT_VARIANTS, "> ".$file_out_prefix."_variants.txt");
-open(OUT_FLIP, "> ".$file_out_prefix."_flip.txt");
 if ($file_in_gvcf =~ /gz$/) {
     open(GVCF, "gunzip -c $file_in_gvcf |") or die "gunzip $file_in_gvcf: $!";
 } else {
@@ -115,21 +125,11 @@ while(<GVCF>){
                                     $variants{$F[0]}{$i} =~ /rs\d+:\d+:(\S+):(\S+)/;
                                     my $a1 = $1;
                                     my $a2 = $2;
-                                    my $flip_a1 = flip($a1);
-                                    my $flip_a2 = flip($a2);
-                                    if ($F[3] eq $a1 || $F[3] eq $a2 || $F[3] eq $flip_a1 || $F[3] eq $flip_a2) {
-                                        if ($F[3] eq $a1 || $F[3] eq $a2) {
-                                            $F[4] = ($F[3] eq $a1) ? $a2 : $a1;
-                                        } else {
-                                            $F[4] = ($F[3] eq $flip_a1) ? $flip_a2 : $flip_a1;
-                                            print OUT_FLIP $variants{$F[0]}{$i}."\n";
-                                        }
+                                    if ($F[3] eq $a1 || $F[3] eq $a2) {
+                                        $F[4] = ($F[3] eq $a1) ? $a2 : $a1;
+                                        $F[1] = $i;
                                         $F[2] = $variants{$F[0]}{$i};
-                                        $F[7] = "";
-                                        $F[8] =~ s/:.+//;
-                                        $F[9] =~ s/:.+//;
-                                        print OUT_VCF join("\t", $F[0], $i, $variants{$F[0]}{$i}, @F[3..(@F-1)])."\n";
-                                        print OUT_VARIANTS $variants{$F[0]}{$i}."\n";
+                                        add_variant_to_output({F=>\@F, OUT_VCF=>\*OUT_VCF, OUT_VARIANTS=>\*OUT_VARIANTS});
                                     }
                                 }
                             }
@@ -142,18 +142,9 @@ while(<GVCF>){
                                     $variants{$F[0]}{$F[1]} =~ /rs\d+:\d+:(\S+):(\S+)/;
                                     my $a1 = $1;
                                     my $a2 = $2;
-                                    my $flip_a1 = flip($a1);
-                                    my $flip_a2 = flip($a2);
-                                    if (($F[3] eq $a1 && $F[4] eq $a2) || ($F[3] eq $a2 && $F[4] eq $a1) || ($F[3] eq $flip_a1 && $F[4] eq $flip_a2) || ($F[3] eq $flip_a2 && $F[4] eq $flip_a1)) {
-                                        if (($F[3] eq $flip_a1 && $F[4] eq $flip_a2) || ($F[3] eq $flip_a2 && $F[4] eq $flip_a1)) {
-                                            print OUT_FLIP $variants{$F[0]}{$F[1]}."\n";
-                                        }
+                                    if (($F[3] eq $a1 && $F[4] eq $a2) || ($F[3] eq $a2 && $F[4] eq $a1)) {
                                         $F[2] = $variants{$F[0]}{$F[1]};
-                                        $F[7] = "";
-                                        $F[8] =~ s/:.+//;
-                                        $F[9] =~ s/:.+//;
-                                        print OUT_VCF join("\t", $F[0], $F[1], $variants{$F[0]}{$F[1]}, @F[3..(@F-1)])."\n";
-                                        print OUT_VARIANTS $variants{$F[0]}{$F[1]}."\n";
+                                        add_variant_to_output({F=>\@F, OUT_VCF=>\*OUT_VCF, OUT_VARIANTS=>\*OUT_VARIANTS});
                                     }
                                 }
                             }
@@ -167,4 +158,7 @@ while(<GVCF>){
 close GVCF;
 close OUT_VCF;
 close OUT_VARIANTS;
+
+# Create empty flip file
+open(OUT_FLIP, "> ".$file_out_prefix."_flip.txt");
 close OUT_FLIP;
