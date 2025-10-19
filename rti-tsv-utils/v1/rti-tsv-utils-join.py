b26e19af016bab795307a5ca2070354a7e5c1580
Nathan Gaddis <ngaddis@rti.org>
2025-10-19 13:54:36 +0000

    Updated rti-tsv-utils-join.py

--- rti-tsv-utils/v1/rti-tsv-utils-join.py
+++ rti-tsv-utils/v1/rti-tsv-utils-join.py
@@ -3,24 +3,44 @@ import pandas as pd
 import pprint as pp
 import sys
 
-def getColListFromArgument(argValue, file, sep):
-    argList = []
-    if (argValue == "" or argValue == "all"):
-        argList = list(pd.read_csv(file, sep=sep, nrows=1))
+def getColumns(file, sep, keepCols=None, removeCols=None):
+    allCols = list(pd.read_csv(file, sep=sep, nrows=1))
+    cols = []
+    if (keepCols is None):
+        cols = allCols.copy()
     else:
-        argList = argValue.split(",")
-    return argList
+        for col in keepCols.split(","):
+            if col in allCols:
+                cols.append(col)
+            else:
+                sys.exit("Column " + col + " not found in " + file)
+    if (removeCols is not None):
+        for col in removeCols.split(","):
+            if col in cols:
+                cols.remove(col)
+            else:
+                sys.exit("Column " + col + " not found in " + file)
+    return cols
 
 # Define arguments
 parser = argparse.ArgumentParser()
 parser.add_argument(
-    "--in-file-left",
+    "--left-file",
+    required = True,
     dest = "left_file",
     type = str,
     help = "Left-hand file for merge"
 )
 parser.add_argument(
-    "--in-file-left-sep",
+    "--left-on",
+    required = True,
+    dest = "left_on",
+    type = str,
+    help = "Field in left file to join on"
+)
+parser.add_argument(
+    "--left-sep",
+    required = False,
     dest = "left_sep",
     default = "whitespace",
     type = str.lower,
@@ -28,50 +48,40 @@ parser.add_argument(
     help = "Left-hand file field separator"
 )
 parser.add_argument(
-    "--in-file-left-cols",
-    dest = "left_cols",
+    "--left-keep-cols",
+    required = False,
+    dest = "left_keep_cols",
     type = str,
-    default = "all",
+    default = None,
     help = "Comma-separated names of columns in left-hand file to include in output"
 )
 parser.add_argument(
-    "--left-on",
-    dest = "left_on",
+    "--left-remove-cols",
+    required = False,
+    dest = "left_remove_cols",
     type = str,
-    help = "Field in left file to join on"
+    default = None,
+    help = "Comma-separated names of columns in left-hand file to remove from output"
 )
 parser.add_argument(
     "--left-suffix",
+    required = False,
     dest = "left_suffix",
     type = str,
     default = "",
     help = "Suffix to add to columns in left file in joined file"
 )
 parser.add_argument(
-    "--in-file-rights",
+    "--right-files",
+    required = True,
     dest = "right_files",
     nargs = "+",
     type = str,
     help = "Right-hand file(s) for merge."
 )
 parser.add_argument(
-    "--in-file-right-seps",
-    dest = "right_seps",
-    nargs = "+",
-    type = str.lower,
-    default = ["whitespace"],
-    help = "Right-hand file field separator(s); Choices: whitespace, tab, space, comma"
-)
-parser.add_argument(
-    "--in-file-right-cols",
-    dest = "right_cols",
-    nargs = "+",
-    type = str,
-    default = ["all"],
-    help = "Column-separated names of columns in right-hand file(s) to include in output"
-)
-parser.add_argument(
     "--right-ons",
+    required = True,
     dest = "right_ons",
     nargs = "+",
     type = str,
@@ -79,22 +89,50 @@ parser.add_argument(
 )
 parser.add_argument(
     "--right-suffixes",
+    required = True,
     dest = "right_suffixes",
     nargs = "+",
     type = str,
-    default = [""],
     help = "Suffix to add to columns in right file in joined file"
 )
 parser.add_argument(
+    "--right-seps",
+    required = False,
+    dest = "right_seps",
+    nargs = "+",
+    type = str.lower,
+    default = ["whitespace"],
+    help = "Right-hand file field separator(s); Choices: whitespace, tab, space, comma"
+)
+parser.add_argument(
+    "--right-keep-cols",
+    required = False,
+    dest = "right_keep_cols",
+    nargs = "+",
+    type = str,
+    default = [None],
+    help = "Column-separated names of columns in right-hand file(s) to include in output"
+)
+parser.add_argument(
+    "--right-remove-cols",
+    required = False,
+    dest = "right_remove_cols",
+    nargs = "+",
+    type = str,
+    default = [None],
+    help = "Column-separated names of columns in right-hand file(s) to remove from output"
+)
+parser.add_argument(
     "--hows",
+    required = True,
     dest = "hows",
     nargs = "+",
     type = str.lower,
-    default = ["inner"],
     help = "Type of joins for each right-hand file; Choices: left, inner"
 )
 parser.add_argument(
-    "--out-file-prefix",
+    "--out-prefix",
+    required = True,
     dest = "out_prefix",
     type = str,
     help = "Prefix for output files"
@@ -115,6 +153,7 @@ sortGroup.add_argument(
 parser.set_defaults(sort=True)
 parser.add_argument(
     "--chunk-size",
+    required = False,
     dest = "chunk_size",
     type = int,
     default = 200000,
@@ -136,58 +175,74 @@ sepRegex = {
 }
 args.left_sep = sepRegex[args.left_sep]
 rightSepRegex = []
-for rightSep in args.right_seps:
-    rightSepRegex.append(sepRegex[rightSep])
+if len(args.right_seps) == 1 and rightCount > 1:
+    for i in range(rightCount):
+        rightSepRegex.append(sepRegex[args.right_seps[0]])
+else:
+    for rightSep in args.right_seps:
+        rightSepRegex.append(sepRegex[rightSep])
 args.right_seps = rightSepRegex
 
 # Check whether arguments are logical
-rightArguments = ["right_cols", "right_ons", "right_seps", "hows", "right_suffixes"]
+rightArguments = ["right_ons", "right_suffixes", "right_keep_cols", "right_remove_cols", "hows"]
 for rightArgument in rightArguments:
     rightArgumentValue = getattr(args, rightArgument)
     rightArgumentCount = len(rightArgumentValue)
-    if rightArgumentCount != rightCount:
-        if rightArgumentCount == 1:
-            setattr(args, rightArgument, [rightArgumentValue[0]] * rightCount)
-        else:
-            sys.exit("Number of arguments provided for " + rightArgument + " do not match number of right files")
-
-# Convert comma-separated arguments to lists
-commaSepArguments = ["left_cols"]
-for commaSepArgument in commaSepArguments:
-    setattr(args, commaSepArgument, getColListFromArgument(getattr(args, commaSepArgument), args.left_file, args.left_sep))
-
-commaSepArguments = ["right_cols"]
-for commaSepArgument in commaSepArguments:
-    argLists = []
-    for rightIndex in range(rightCount):
-        arg = getattr(args, commaSepArgument)[rightIndex]
-        argLists.append(getColListFromArgument(arg, args.right_files[rightIndex], args.right_seps[rightIndex]))
-    setattr(args, commaSepArgument, argLists)
+    if rightArgumentCount == 1 and rightCount > 1:
+        setattr(args, rightArgument, [rightArgumentValue[0]] * rightCount)
+    elif rightArgumentCount != rightCount:
+        sys.exit(
+            "Number of values for --" + rightArgument.replace("_", "-") +
+            " (" + str(rightArgumentCount) + ") does not match number of right files (" + str(rightCount) + ")"
+        )
+    else:
+        setattr(args, rightArgument, rightArgumentValue)
 
 # Open log file
 fileLog = args.out_prefix + ".log"
 logHandle = open(fileLog, 'w')
 logHandle.write("Script: rti-tsv-utils-join.py\n")
 logHandle.write("Arguments:\n")
+
 # Write arguments to log file
 logText = pp.PrettyPrinter(indent = 4)
 logHandle.write(logText.pformat(vars(args)))
 logHandle.write("\n\n")
 logHandle.flush()
 
-# Create list of columns to output
-outCols = [leftCol + args.left_suffix for leftCol in args.left_cols]
-for rightIndex in range(rightCount):
-    outCols = outCols + [rightCol + args.right_suffixes[rightIndex] for rightCol in args.right_cols[rightIndex]]
-    rightOn = args.right_ons[rightIndex] + args.right_suffixes[rightIndex]
-    outCols.remove(rightOn)
+# Get left columns to keep
+leftCols = getColumns(
+    args.left_file,
+    args.left_sep,
+    keepCols = args.left_keep_cols,
+    removeCols = args.left_remove_cols
+)
 
+# Get right columns to keep
+rightCols = []
+for rightIndex in range(rightCount):
+    rightCols.append(getColumns(
+        args.right_files[rightIndex],
+        args.right_seps[rightIndex],
+        keepCols = args.right_keep_cols[rightIndex] if rightIndex < len(args.right_keep_cols) else None,
+        removeCols = args.right_remove_cols[rightIndex] if rightIndex < len(args.right_remove_cols) else None
+    ))
+
+# Create list of output columns
+outCols = leftCols.copy()
+if args.left_suffix != "":
+    outCols = [(outCol + '_' + args.left_suffix) for outCol in outCols]
+for rightIndex in range(rightCount):
+    outCols = outCols + [(rightCol + '_' + args.right_suffixes[rightIndex]) for rightCol in rightCols[rightIndex]]
+    rightOn = args.right_ons[rightIndex] + '_' + args.right_suffixes[rightIndex]
+    outCols.remove(rightOn)
+    
 # Read left file, adding columns from right files
 chunkCount = 1
 for left in pd.read_table(
     args.left_file,
     sep = args.left_sep,
-    usecols = args.left_cols,
+    usecols = leftCols,
     chunksize = args.chunk_size
 ):
     logHandle.write("Processing chunk " + str(chunkCount) + " of " + args.left_file + "\n")
@@ -199,41 +254,41 @@ for left in pd.read_table(
     # Remove duplicates
     out.drop_duplicates(subset=[args.left_on], inplace = True)
 
-    # Get count of rows in out dataframe
-    outCount = out.shape[0]
-
     # Add suffixes to column names
     out = out.add_suffix(args.left_suffix)
     leftOn = args.left_on + args.left_suffix
 
     # Loop through right files
-    firstMerge = True
     for rightIndex in range(rightCount):
         logHandle.write("Reading " + args.right_files[rightIndex] + "\n")
         logHandle.flush()
-
-        # Create dataframe for right file rows to save
-        dfRight = pd.DataFrame(columns=args.right_cols[rightIndex])
 
         # Create iterator for right file
         for right in pd.read_table(
             args.right_files[rightIndex],
             sep = args.right_seps[rightIndex],
-            usecols = args.right_cols[rightIndex],
+            usecols = rightCols[rightIndex],
             chunksize = args.chunk_size
         ):
+            nextRight = right[right[args.right_ons[rightIndex]].isin(out[leftOn])]
+            nextRight = nextRight[rightCols[rightIndex]]
+            
             # Capture rows from right file whose keys are present in the left file
-            dfRight = dfRight.append(right[right[args.right_ons[rightIndex]].isin(out[leftOn])])
+            if 'dfRight' not in locals():
+                dfRight = nextRight
+            else:
+                dfRight = pd.concat([dfRight, nextRight], ignore_index=True)
 
             # Remove duplicates
             dfRight.drop_duplicates(subset=[args.right_ons[rightIndex]], inplace = True)
 
             # Check if all keys in out accounted for
-            dfRightCount = dfRight.shape[0]
+            if out[args.left_on].isin(dfRight[args.right_ons[rightIndex]]).all():
+                break
 
         # Add suffixes to column names
-        dfRight = dfRight.add_suffix(args.right_suffixes[rightIndex])
-        rightOn = args.right_ons[rightIndex] + args.right_suffixes[rightIndex]
+        dfRight = dfRight.add_suffix('_' + args.right_suffixes[rightIndex])
+        rightOn = args.right_ons[rightIndex] + '_' + args.right_suffixes[rightIndex]
 
         # Merge out and dfRight dataframes
         logHandle.write("Merging " + args.right_files[rightIndex] + "\n")
@@ -245,7 +300,7 @@ for left in pd.read_table(
             right_on = rightOn,
             sort = args.sort
         )
-
+        print(out.head())
         del dfRight
 
     logHandle.write("Writing chunk " + str(chunkCount) + "\n")
