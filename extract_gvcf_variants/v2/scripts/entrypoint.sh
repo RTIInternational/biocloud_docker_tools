f75f46d864889cb6168da33be5e884f5efa31b37
Nathan Gaddis <ngaddis@rti.org>
2026-02-25 20:02:21 +0000

    Troubleshooting extract_gvcf_variants

--- extract_gvcf_variants/v2/scripts/entrypoint.sh
+++ extract_gvcf_variants/v2/scripts/entrypoint.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 args_array=("$@")
 
+if [ -z "$wf_definition" ]; then
+    echo "wf_definition not provided, exiting!"
+    exit 
+fi
 if [ -z "$wf_arguments" ]; then
     echo "wf_arguments not provided, exiting!"
     exit 
-else
-    /opt/extract_gvcf_variants.pl \
-        --args $wf_arguments
 fi
+
+python3 /pipeline/scripts/run_pipeline.py \
+    --wf_definition /pipeline/config/$wf_definition.json \
+    --wf_tasks /pipeline/config/tasks.json \
+    --wf_arguments $wf_arguments
